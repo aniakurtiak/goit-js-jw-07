17b839760553e170173b5d5b6328aea3e58c3239
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-05 23:46:15 +0300

    task-01-step1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+console.log(list);
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`)
+        .join('');
+}
+list.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
