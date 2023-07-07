e816a3c9c1a26db9474c34028f737a8aa9d64893
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-07 04:00:13 +0300

    01-gallery-done!

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,6 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const list = document.querySelector('.gallery');
-// const hideModal = document.querySelector('.');
 
 function createMarkup(arr) {
     return arr.map(({ preview, original, description }) => `
@@ -21,38 +20,25 @@ function createMarkup(arr) {
         .join('');
 }
 list.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
-
 list.addEventListener('click', handllerClickGallery);
 
 function handllerClickGallery(evt) {
     evt.preventDefault();
-    console.log(evt.target);
-    // const imageItem = evt.target.closest('.gallery__item');
-
-    const obj = findImageItem(evt.target);
-    createOriginalMarkup(obj);
-
-
-// НЕ ЗНАЮ ЧИ ТРЕБА ПЕРЕВІРКИ НИЖЧЕ
-    // if (imageItem) {
-    //     const id = imageItem.getAttribute(alt);
-    //     console.log(id);
-    // }
-
+    if (evt.target.closest('.gallery__item')) {
+        const obj = findImageItem(evt.target);
+        createOriginalModal(obj);
+    }
 }
+
 function findImageItem(item) {
  const id = item.getAttribute('alt');
-    console.log(id);
     const currentImage = galleryItems.find(({ description }) => description === id);
     return currentImage;
 }
 
-function createOriginalMarkup({original, description }= {}) {
+function createOriginalModal({original, description }= {}) {
 const instance = basicLightbox.create(`
-    <div class="modal">
        <img src="${original}" alt="${description}"> 
-    </div>
 `)
     return instance.show()
 }
-
