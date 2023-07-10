374e6326df5c70f1cd74dee1803099e3b0b0cb9a
aniakurtiak <124797307+aniakurtiak@users.noreply.github.com>
2023-07-11 01:16:58 +0300

    task02-done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -15,3 +15,23 @@ function createMarkup(arr) {
 }
 
 list.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+list.addEventListener('click', handllerClickGallery);
+
+function handllerClickGallery(evt) {
+    evt.preventDefault();
+    if (evt.target.closest('.gallery__item')) {
+        const obj = findImageItem(evt.target);
+        createOriginalModal(obj);
+    }
+}
+
+function findImageItem(item) {
+ const id = item.getAttribute('alt');
+    const currentImage = galleryItems.find(({ description }) => description === id);
+    return currentImage;
+}
+
+function createOriginalModal() {
+    const lightbox = new SimpleLightbox('.gallery a', { captionsData: `alt`, captionDelay: 250 });
+    return lightbox;
+}
